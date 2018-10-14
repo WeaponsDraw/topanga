3e55bb6c275e473b0a09ce9efec60ea2ed0f6587
Nicu Parente <nicuparente@gmail.com>
2018-10-14 08:40:35 -0700

    respose

--- public/src/component/App.js
+++ public/src/component/App.js
@@ -2,13 +2,17 @@ import React, { Component, Fragment } from "react";
 import { BrowserRouter, Route, Link } from "react-router-dom";
 
 import Site from './Site';
+import RSVP from './RSVP'
 import Admin from './Admin';
+import ResponseLanding from './ResponseLanding'
+
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
         <Fragment>
           <Route exact path="/nalakaiser" component={Site} />
+          <Route exact path="/nalakaiser/rsvp/:id" component={ResponseLanding} />
           <Route exact path="/nalakaiser/admin" component={Admin} />
         </Fragment>
       </BrowserRouter>
